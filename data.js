e4f2b36953f82efdfb64f0a848489aba5c91056c
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-08-07 20:01:23 +0600

    first commit

---
+++ data.js
@@ -0,0 +1,31 @@
+// 1. Student Data (in data.js)
+
+// Create an array of at least 4 students
+
+// Each student must have: id, name, scores (array of 3 test scores)
+
+// Export this data so other files can use it
+
+let id = 1;
+export const students = [
+    {
+        id: 1,
+        name: "Mr ovro",
+        scores: [85, 90, 88]
+    },
+    {
+        id: 2,
+        name: "Mr Tausif",
+        scores: [92, 88, 91]
+    },
+    {
+        id: 3,
+        name: "Mr Arfat",
+        scores: [78, 85, 82]
+    },
+    {
+        id: 4,
+        name: "Jill Johnson",
+        scores: [88, 92, 89]
+    }
+]
---
+++ function.js
@@ -0,0 +1,32 @@
+import { students } from "./data.js";
+
+export const addStudent = (name, scores) => {
+   
+}
+
+
+export const calculateAverage = (id) => {
+
+}
+
+
+export const getAllAverage = () => {
+
+}
+
+
+export const getTopPerformers = () => {
+
+}
+
+export const getSummary = () => {
+
+}
+
+export const updateScore = (id, newScore) => {
+
+}
+
+export const printAllStudents = () => {
+
+}
