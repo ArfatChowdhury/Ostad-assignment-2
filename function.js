52fa12a32f28d1b4b0ff396435ae13ddf6774fed
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-08-07 20:27:59 +0600

    create calculateAvarage function

--- function.js
+++ function.js
@@ -13,7 +13,15 @@ export const addStudent = (name, scores) => {
 
 
 export const calculateAverage = (id) => {
-
+    const student = students.find(student => student.id === id);
+    if (!student) {
+        console.warn("Student not found");
+        return;
+    }
+    //i take student and then i take scores and then i take sum of scores and then i take average of scores
+   const average = student.scores.reduce((sum, score) => sum + score, 0) / student.scores.length;
+   console.log(`Average score for ${student.name} is ${average}`);
+    return average;
 }
 
 
